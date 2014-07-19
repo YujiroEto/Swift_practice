7ee0525db9a5066bc6a0fd0fbfaed584d1ec4430
Yujiro Eto <yujro.eto@gmail.com>
2014-07-19 15:26:12 +0900

    first commit

---
+++ Hello World/Hello World/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Hello World
+//
+//  Created by Yujiro Eto on 2014/07/19.
+//  Copyright (c) 2014年 Yujiro Eto. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
